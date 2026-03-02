44fe461d12f7bcf7fa1a6efe3e580a4c2f04af55
Pausan Chaudhary <Poushan.chy12@gmail.com>
2026-03-02 22:27:45 +0545

    ne

--- src/config/database.js
+++ src/config/database.js
@@ -1,34 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import mainConfig from './index.js';    
 
-let isConnected = false;
+async function connectToDatabase() {
 
-const connectToDatabase = async () => {
-  if (isConnected && mongoose.connection.readyState === 1) {
-    return mongoose.connection;
-  }
-
-  const uri = process.env.MONGODB_URL || process.env.MONGODB_URI;
-  if (!uri) {
-    throw new Error('MONGODB_URL or MONGODB_URI environment variable is required');
-  }
-
-  try {
-    console.log('Connecting to MongoDB:', uri);
-    const db = await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 10000,
-      retryWrites: true,
-      maxPoolSize: 10,
-      minPoolSize: 2,
-    });
-
-    isConnected = db.connections[0].readyState === 1;
-    console.log('MongoDB Connected');
-    return db.connection;
-  } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    throw err;
-  }
-};
+    try {
+    const status = await mongoose.connect(mainConfig.mongoDBURL,);
+        console.log('Connected to MongoDB:', status.connection.host);
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+        throw error;
+    }
+}
 
 export default connectToDatabase;
