94cf8da4de36cb88b36ce82904c74a6f5d112018
Pausan Chaudhary <Poushan.chy12@gmail.com>
2026-03-07 18:56:26 +0545

    Remove Admin restriction and undefined import from artRoute

--- src/routes/artRoute.js
+++ src/routes/artRoute.js
@@ -14,7 +14,7 @@ const upload = multer({storage: multer.memoryStorage()});
 
 
 
-router.get('/', roleBasedAuth(Admin), artController.getArt);
+router.get('/', artController.getArt);
 router.get('/:id', artController.getArtById);
 
 router.post(
